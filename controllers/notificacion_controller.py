737167e9215e8076103a0a1609f6f75c9281907e
sebas12061 <guevara.psebastian@gmail.com>
2025-09-09 09:54:18 -0500

    Nuevas funcionalidades agregadas

--- controllers/notificacion_controller.py
+++ controllers/notificacion_controller.py
@@ -16,3 +16,20 @@ def crear_notificacion():
 @notificacion_bp.route("/notificaciones", methods=["GET"])
 def listar_notificaciones():
     return jsonify(service.obtener_notificaciones()), 200
+
+
+# Actualizar notificaciones como leídas
+@notificacion_bp.route("/notificaciones/<int:notificacion_id>/leer", methods=["PUT"])
+def notificacion_leida(notificacion_id):
+    actualizada = service.notificacion_leida(notificacion_id)
+    if actualizada:
+        return jsonify(actualizada), 200
+    return jsonify({"error": "Notificación no encontrada"}), 404
+
+# Eliminar notificaciones
+@notificacion_bp.route("/notificaciones/<int:notificacion_id>", methods=["DELETE"])
+def eliminar_notificacion(notificacion_id):
+    eliminada = service.eliminar_notificacion(notificacion_id)
+    if eliminada:
+        return jsonify({"mensaje": "Notificación eliminada"}), 200
+    return jsonify({"error": "Notificación no encontrada"}), 404
